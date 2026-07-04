492efdf1c5fe2bc8cbd64db315f6b235c74bd880
AdriannaWierzowiecka <wierzowiecka.adrianna@gmail.com>
2026-07-04 11:36:25 +0200

    add dockerr, synchro project

--- application/angular/src/main.ts
+++ application/angular/src/main.ts
@@ -1,4 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { PackagingTrizToolComponent } from './app/components/packaging-triz-tool/packaging-triz-tool.component';
 
-bootstrapApplication(PackagingTrizToolComponent).catch((err) => console.error(err));
+bootstrapApplication(PackagingTrizToolComponent, appConfig).catch((err) => console.error(err));
